d790e90e363b7999a57ca796e07a5db728613bc5
Devlopriax <72613352+SylvainAsselot@users.noreply.github.com>
2020-11-23 15:07:41 +0100

    first commit
    tp

---
+++ assets/js/main.js
@@ -0,0 +1,488 @@
+/**
+* Template Name: MyResume - v2.1.0
+* Template URL: https://bootstrapmade.com/free-html-bootstrap-template-my-resume/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+
+var channel_max = 10;										// number of channels
+  audiochannels = new Array();
+  for (a=0;a<channel_max;a++) {									// prepare the channels
+    audiochannels[a] = new Array();
+    audiochannels[a]['channel'] = new Audio();						// create a new audio object
+    audiochannels[a]['finished'] = -1;							// expected end time for this channel
+  }
+  function play_multi_sound(s) {
+    for (a=0;a<audiochannels.length;a++) {
+      thistime = new Date();
+      if (audiochannels[a]['finished'] < thistime.getTime()) {			// is this channel finished?
+        audiochannels[a]['finished'] = thistime.getTime() + document.getElementById(s).duration*1000;
+        audiochannels[a]['channel'].src = document.getElementById(s).src;
+        audiochannels[a]['channel'].load();
+        audiochannels[a]['channel'].play();
+        break;
+      }
+    }
+  }
+!(function($) {
+  "use strict";
+
+  // Preloader
+  $(window).on('load', function() {
+    if ($('#preloader').length) {
+      $('#preloader').delay(100).fadeOut('slow', function() {
+        $(this).remove();
+      });
+    }
+  });
+
+  // Hero typed
+  if ($('.typed').length) {
+    var typed_strings = $(".typed").data('typed-items');
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+    
+  }
+  if ($('.typed2').length) {
+    var typed_strings = $(".typed2").data('typed-items');
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed2', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+    
+  }
+  // Smooth scroll for the navigation menu and links with .scrollto classes
+  $(document).on('click', '.nav-menu a, .scrollto', function(e) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      if (target.length) {
+        e.preventDefault();
+
+        var scrollto = target.offset().top;
+
+        $('html, body').animate({
+          scrollTop: scrollto
+        }, 1500, 'easeInOutExpo');
+
+        if ($(this).parents('.nav-menu, .mobile-nav').length) {
+          $('.nav-menu .active, .mobile-nav .active').removeClass('active');
+          $(this).closest('li').addClass('active');
+        }
+
+        if ($('body').hasClass('mobile-nav-active')) {
+          $('body').removeClass('mobile-nav-active');
+          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+        }
+        return false;
+      }
+    }
+  });
+
+  // Activate smooth scroll on page load with hash links in the url
+  $(document).ready(function() {
+    if (window.location.hash) {
+      var initial_nav = window.location.hash;
+      if ($(initial_nav).length) {
+        var scrollto = $(initial_nav).offset().top;
+        $('html, body').animate({
+          scrollTop: scrollto
+        }, 1500, 'easeInOutExpo');
+      }
+    }
+  });
+
+  $(document).on('click', '.mobile-nav-toggle', function(e) {
+    $('body').toggleClass('mobile-nav-active');
+    $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+  });
+
+  $(document).click(function(e) {
+    var container = $(".mobile-nav-toggle");
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+      if ($('body').hasClass('mobile-nav-active')) {
+        $('body').removeClass('mobile-nav-active');
+        $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+      }
+    }
+  });
+
+  // Navigation active state on scroll
+  var nav_sections = $('section');
+  var main_nav = $('.nav-menu, #mobile-nav');
+
+  $(window).on('scroll', function() {
+    var cur_pos = $(this).scrollTop() + 300;
+
+    nav_sections.each(function() {
+      var top = $(this).offset().top,
+        bottom = top + $(this).outerHeight();
+
+      if (cur_pos >= top && cur_pos <= bottom) {
+        if (cur_pos <= bottom) {
+          main_nav.find('li').removeClass('active');
+        }
+        main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
+      }
+      if (cur_pos < 200) {
+        $(".nav-menu ul:first li:first").addClass('active');
+      }
+    });
+  });
+
+  // Back to top button
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+
+  $('.back-to-top').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+  });
+
+  // jQuery counterUp
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 1000
+  });
+
+  // Skills section
+  $('.skills-content').waypoint(function() {
+    $('.progress .progress-bar').each(function() {
+      $(this).css("width", $(this).attr("aria-valuenow") + '%');
+    });
+  }, {
+    offset: '80%'
+  });
+
+  // Init AOS
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      once: true
+    });
+  }
+
+  // Porfolio isotope and filter
+  $(window).on('load', function() {
+    var portfolioIsotope = $('.portfolio-container').isotope({
+      itemSelector: '.portfolio-item'
+    });
+
+    $('#portfolio-flters li').on('click', function() {
+      $("#portfolio-flters li").removeClass('filter-active');
+      $(this).addClass('filter-active');
+
+      portfolioIsotope.isotope({
+        filter: $(this).data('filter')
+      });
+      aos_init();
+    });
+
+    // Initiate venobox (lightbox feature used in portofilo)
+    $('.venobox').venobox({
+      'share': false
+    });
+
+    // Initiate aos_init() function
+    aos_init();
+
+  });
+
+  // Testimonials carousel (uses the Owl Carousel library)
+  $(".testimonials-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    items: 1
+  });
+
+  // Portfolio details carousel
+  $(".portfolio-details-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    items: 1
+  });
+  
+// Mots croisés c'est une erreur qui arrive, attention les yeux
+
+var grid = [	['1', 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ['1', 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ['1', 0, 0, 0, 0, 0, 0, 0, 0, 0],    
+  ['1', 0, 0, 0, '2', 0, 0, 0, 0, 0],
+  ['1', 0, 0, 0, '2', 0, 0, 0, 0, '3'],
+  ['1', 0, 0, 0, '2', 0, 0, 0, 0, '3'],
+  ['1', 0, 0, 0, '2', 0, '4,5', '5', '5', '3,5'],
+  ['1,6', '6', '6', '6', '2,6', '6', '4,6', 0, 0, '3'],
+  ['1', 0, 0, 0, '2', 0, '4', 0, 0, '3'],
+  [0, 0, 0, 0, '2', 0, '4', 0, 0, '3'],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ 
+			];
+
+var clues = [" Copyright du template",
+             " Notre nom, élèves de Nuum Factory",
+             " <>",
+             " En anglais, Zidane mis 2 buts en 1998 de cette manière. (et va de paire avec <'body'>)",
+             " Le squelette d'un site",
+             " La qualité principale d'un élève de The Nuum Factory"
+            ];
+
+var answers = ["bootstrap",
+             "nuumies",
+             "balise",
+             "head",
+             "html",
+             "agilite"
+            ];
+
+//Draw grid
+$.each(grid,function(i){
+    var row = $('<tr></tr>');
+	$.each(this,function(j){
+        if(this == 0){
+        	$(row).append('<td class="square empty"></td>');  
+        }
+        else{
+            var question_number = String(grid[i][j]).split(",");
+            
+            var starting_number = '';
+            var question_number_span = '';
+            
+            for(var k = 0;k < question_number.length;k++){
+                var direction = get_direction(question_number[k]);
+                var startpos = get_startpos(question_number[k],direction);
+                
+                if(direction == "horizontal" && startpos[0] == i && startpos[1] == j){
+                    starting_number += question_number[k]+",";
+                                
+                }
+                else if(direction == "vertical" && startpos[0] == j && startpos[1] == i){
+                    starting_number += question_number[k]+",";
+                }              
+                
+            }
+            if(starting_number != ""){
+                question_number_span = '<span class="question_number">'+starting_number.replace(/(^,)|(,$)/g, "")+'</span>';   
+            }
+            
+            $(row).append('<td>'+question_number_span+'<div class="square letter" data-number="'+this+'" contenteditable="true"></div></td>'); 
+        }
+    });
+    $("#puzzle").append(row);
+});
+
+//Draw hints
+var vertical_hints = $('<div id="vertical_hints"></div>');
+var horizontal_hints = $('<div id="horizontal_hints"></div>');
+$.each(clues,function(index){
+    
+    var direction = get_direction(index+1);
+    
+    if(direction == "horizontal"){
+        $(horizontal_hints).append('<div class="hint"><b>'+(index+1)+'</b>.-'+clues[index]+'</hint>');
+    }
+    else if(direction == "vertical"){
+    	$(vertical_hints).append('<div class="hint"><b>'+(index+1)+'</b>.-'+clues[index]+'</hint>');
+    }
+});
+$("#vertical_hints_container").append(vertical_hints);
+$("#horizontal_hints_container").append(horizontal_hints);
+
+$(".letter").keyup(function(){
+    var this_text = $(this).text();
+    if(this_text.length > 1){
+    	$(this).text(this_text.slice(0,1));
+    }
+});
+
+$(".letter").click(function(){
+    document.execCommand('selectAll',false,null);
+    
+    $(".letter").removeClass("active");
+    $(this).addClass("active");
+    
+    $(".hint").css("color","initial");
+    
+    var question_numbers = String($(this).data("number")).split(",");
+    
+    $.each(question_numbers,function(){
+        $("#hints .hint:nth-child("+this+")").css("color","red");
+    });
+});
+
+$("#solve").click(function(){
+    if(!$(".letter.active").length)
+       return;
+	var question_numbers = String($(".letter.active").data("number")).split(",");
+    $.each(question_numbers,function(){
+        fillAnswer(this);
+    });
+});
+
+$("#clear_all").click(function(){
+    if(!$(".letter.active").length)
+       return;
+	var question_numbers = String($(".letter.active").data("number")).split(",");
+    $.each(question_numbers,function(){
+        clearAnswer(this);
+    });
+});
+
+$("#check").click(function(){
+    $("#puzzle td div").css("color","initial");
+    for(var i = 0;i < answers.length;i++){
+        checkAnswer(i+1);
+    }    
+});
+
+$("#clue").click(function(){
+    if(!$(".letter.active").length)
+       return;
+	var question_numbers = String($(".letter.active").data("number")).split(",");
+    showClue(question_numbers[0],$(".letter.active").parent().index(),$(".letter.active").parent().parent().index());
+});
+
+function get_direction(question_number){
+    for(var i=0;i < grid.length;i++){
+    	for(var j=0;j < grid[i].length;j++){
+            if(String(grid[i][j]).indexOf(question_number) != -1){            
+                if(grid[i+1][j] == question_number || grid[i-1][j] == question_number){
+                    return "vertical";
+                }
+
+                if(grid[i][j+1] == question_number || grid[i][j-1] == question_number){
+                    return "horizontal";
+                }
+            }
+    	}
+    }
+}
+    
+function get_startpos(question_number,direction){
+	if(direction == "horizontal"){
+       for(var i=0;i < grid.length;i++){
+            for(var j=0;j < grid[i].length;j++){
+                if(String(grid[i][j]).indexOf(question_number) != -1){            
+                    return [i,j];
+                }
+            }
+        }
+    }
+    
+    else if(direction == "vertical"){
+       for(var i=0;i < grid.length;i++){
+            for(var j=0;j < grid[i].length;j++){
+                if(String(grid[j][i]).indexOf(question_number) != -1){            
+                     return [i,j];
+                }
+            }
+        }
+    }
+}
+    
+function fillAnswer(question_number){
+    $("#puzzle td div").css("color","initial");
+    
+    var question_answer = answers[question_number-1];
+    var direction = get_direction(question_number);
+    var startpos = get_startpos(question_number,direction);
+    var answer_letters = question_answer.split("");
+    
+    if(direction == "horizontal"){
+        for(var i = 0; i < answer_letters.length; i++){
+            $("#puzzle tr:nth-child("+(startpos[0]+1)+") td:nth-child("+(startpos[1]+1+i)+") div").text(answer_letters[i]);
+        }
+    	 
+    }
+    else if(direction == "vertical"){
+        for(var i = 0; i < answer_letters.length; i++){
+            $("#puzzle tr:nth-child("+(startpos[1]+1+i)+") td:nth-child("+(startpos[0]+1)+") div").text(answer_letters[i]);
+        }
+    	 
+    }
+}
+
+function clearAnswer(question_number){
+    $("#puzzle td div").css("color","initial");
+    
+    var question_answer = answers[question_number-1];
+    var direction = get_direction(question_number);
+    var startpos = get_startpos(question_number,direction);
+    var answer_letters = question_answer.split("");
+    
+    if(direction == "horizontal"){
+        for(var i = 0; i < answer_letters.length; i++){
+            $("#puzzle tr:nth-child("+(startpos[0]+1)+") td:nth-child("+(startpos[1]+1+i)+") div").text('');
+        }
+    	 
+    }
+    else if(direction == "vertical"){
+        for(var i = 0; i < answer_letters.length; i++){
+            $("#puzzle tr:nth-child("+(startpos[1]+1+i)+") td:nth-child("+(startpos[0]+1)+") div").text('');
+        }
+    	 
+    }
+}
+
+function checkAnswer(question_number){
+    var question_answer = answers[question_number-1];
+    var direction = get_direction(question_number);
+    var startpos = get_startpos(question_number,direction);
+    var answer_letters = question_answer.split("");
+    
+    if(direction == "horizontal"){
+        for(var i = 0; i < answer_letters.length; i++){
+            if($("#puzzle tr:nth-child("+(startpos[0]+1)+") td:nth-child("+(startpos[1]+1+i)+") div").text() != question_answer[i] && $("#puzzle tr:nth-child("+(startpos[0]+1)+") td:nth-child("+(startpos[1]+1+i)+") div").text() != ""){
+                $("#puzzle tr:nth-child("+(startpos[0]+1)+") td:nth-child("+(startpos[1]+1+i)+") div").css("color","red");
+            }
+        }
+    	 
+    }
+    else if(direction == "vertical"){
+        for(var i = 0; i < answer_letters.length; i++){
+            if($("#puzzle tr:nth-child("+(startpos[1]+1+i)+") td:nth-child("+(startpos[0]+1)+") div").text() != question_answer[i] && $("#puzzle tr:nth-child("+(startpos[1]+1+i)+") td:nth-child("+(startpos[0]+1)+") div").text() != ""){
+                $("#puzzle tr:nth-child("+(startpos[1]+1+i)+") td:nth-child("+(startpos[0]+1)+") div").css("color","red");
+            }
+        }
+    	 
+    }
+}
+
+function showClue(question_number,i,j){
+    var question_answer = answers[question_number-1];
+    var direction = get_direction(question_number);
+    var startpos = get_startpos(question_number,direction);
+    var answer_letters = question_answer.split("");
+    
+    if(direction == "horizontal"){
+        $("#puzzle tr:nth-child("+(j+1)+") td:nth-child("+(i+1)+") div").text(answer_letters[i - startpos[1]]).css("color","initial");
+    }
+    else if(direction == "vertical"){
+        $("#puzzle tr:nth-child("+(j+1)+") td:nth-child("+(i+1)+") div").text(answer_letters[j - startpos[1]]).css("color","initial");
+    }
+}
+
+
+
+
+// fin mots croisés
+
+
+})(jQuery);
